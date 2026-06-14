730a763a27d93905dadeeddc9fc2e1654520edb9
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-06-14 11:45:58 +0200

    add AppDbContext for database interaction and logging

---
+++ nerv.log/Database/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nerv.log.Model;
+
+namespace nerv.log.Database;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<LogEntry> Logs => Set<LogEntry>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<LogEntry>()
+            .HasIndex(x => new { x.TimeStamp, x.ServiceName });
+    }
+}
