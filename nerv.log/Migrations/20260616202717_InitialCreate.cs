ed3d42c7f96454336f57539fca03b21ab2d1dec4
pazurkota <piotrek.jelonek@icloud.com>
2026-06-16 22:32:14 +0200

    Update database connection settings and add initial migration for log storage

---
+++ nerv.log/Migrations/20260616202717_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace nerv.log.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    level = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    service_name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    message = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Logs_timestamp_service_name",
+                table: "Logs",
+                columns: new[] { "timestamp", "service_name" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+        }
+    }
+}
