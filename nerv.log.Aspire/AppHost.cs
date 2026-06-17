119f5d457d3a91a63b40c9a6728b21c666a7bd0a
pazurkota <piotrek.jelonek@icloud.com>
2026-06-17 12:23:12 +0200

    Add PostgreSQL server and database setup in AppHost

--- nerv.log.Aspire/AppHost.cs
+++ nerv.log.Aspire/AppHost.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgresServer = builder.AddPostgres("postgres-server")
+    .WithDataVolume()
+    .WithPgAdmin();
+
+var postgresDb = postgresServer.AddDatabase("nerv-log-db");
+
+var server = builder.AddDockerfile("nerv-log-server", 
+        "../", "../nerv.log.Server/Dockerfile")
+    .WithReference(postgresDb);
+
 builder.Build().Run();
