5c49de9d2de89d9e204e7fcb9c091d40ed07a197
pazurkota <piotrek.jelonek@icloud.com>
2026-06-17 11:49:46 +0200

    Implement dynamic scaling for LogStorageWorker using EnvService for worker management

--- nerv.log.Server/Workers/LogStorageWorker.cs
+++ nerv.log.Server/Workers/LogStorageWorker.cs
@@ -1,36 +1,76 @@
+using System.Collections.Concurrent;
 using System.Threading.Channels;
 using Microsoft.EntityFrameworkCore;
 using nerv.log.Database;
 using nerv.log.Model;
+using nerv.log.Services;
 
 namespace nerv.log.Workers;
 
 public class LogStorageWorker
         (Channel<LogEntry> channel, 
         IDbContextFactory<AppDbContext> contextFactory,
-        ILogger<LogStorageWorker> logger) : BackgroundService
+        ILogger<LogStorageWorker> logger,
+        EnvService envService) : BackgroundService
 {
-    private const int BatchSize = 1000;
-    private const int WorkersCount = 4; // temp only
-    
-    // scaling thresholds
+    private readonly ConcurrentDictionary<int, CancellationTokenSource> _activeWorkers = new();
+    private int _workerIdCounter = 0;
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        logger.LogInformation("worker: Multi-thread saving is available. Workers count: {count}", WorkersCount);
+        logger.LogInformation("worker: Worker scale orchestration available. Min: {min}, Max, {max}",
+            envService.MinWorkers, envService.MaxWorkers);
 
-        var workers = new Task[WorkersCount];
-
-        for (int i = 0; i < WorkersCount; i++)
+        for (int i = 0; i < envService.MinWorkers; i++)
         {
-            int workerId = i + 1;
-            workers[i] = Task.Run(() => StartWorkerAsync(workerId, cancellationToken: stoppingToken), stoppingToken);
+            ScaleUp(cancellationToken: stoppingToken);
         }
 
-        await Task.WhenAll(workers);
-        logger.LogInformation("worker: All save threads has been stopped");
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(1000, stoppingToken);
+
+            int currentQueueSize = channel.Reader.Count;
+            int currentWorkerCount = _activeWorkers.Count;
+
+            if (currentQueueSize > currentWorkerCount * envService.ThresholdByWorker
+                && currentWorkerCount < envService.MaxWorkers)
+            {
+                ScaleUp(cancellationToken: stoppingToken);
+            } 
+            else if (currentQueueSize < (currentWorkerCount - 1) * envService.ThresholdByWorker
+                     && currentWorkerCount > envService.MinWorkers)
+            {
+                ScaleDown();
+            }
+        }
+
+        foreach (var workersValue in _activeWorkers.Values) await workersValue.CancelAsync();
     }
 
+    private void ScaleUp(CancellationToken cancellationToken)
+    {
+        int id = Interlocked.Increment(ref _workerIdCounter);
+        var workerCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+        if (_activeWorkers.TryAdd(id, workerCts))
+        {
+            logger.LogWarning("worker: scaling up: Running worker #{id}", id);
+            Task.Run(() => StartWorkerAsync(id, workerCts.Token), workerCts.Token);
+        }
+    }
+
+    private void ScaleDown()
+    {
+        var firstWorker = _activeWorkers.Keys.FirstOrDefault(id => id > envService.MinWorkers);
+
+        if (firstWorker == 0 || !_activeWorkers.TryRemove(firstWorker, out var workerCts)) return;
+        
+        logger.LogWarning("worker: scaling down: Closing worker #{id}", firstWorker);
+        workerCts.Cancel();
+        workerCts.Dispose();
+    }
+    
     private async Task StartWorkerAsync(int workerId, CancellationToken cancellationToken)
     {
         logger.LogInformation("worker #{id}: ready to work.", workerId);
@@ -46,7 +86,7 @@ public class LogStorageWorker
                 {
                     batch.Add(log);
 
-                    if (batch.Count >= BatchSize)
+                    if (batch.Count >= envService.BatchSize)
                     {
                         await FlushBatchToDatabaseAsync(workerId, batch);
                     }
