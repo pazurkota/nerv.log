08207dc34017d1eb962e320efcfbb872e63c2268
pazurkota <piotrek.jelonek@icloud.com>
2026-06-22 13:54:43 +0200

    Add CLI command configuration for TestAgent in CliConfig

--- nerv.log.Cli/CliConfig.cs
+++ nerv.log.Cli/CliConfig.cs
@@ -1,6 +1,24 @@
+using nerv.log.Cli.Commands;
+using Spectre.Console.Cli;
+
 namespace nerv.log.Cli;
 
 public class CliConfig
 {
-    
+    public static void ConfigureCommands(IConfigurator config)
+    {
+        config.SetApplicationName("nerv.log");
+        config.SetApplicationVersion("v0.2");
+
+#if DEBUG
+        config.PropagateExceptions();
+#endif
+
+        config.AddBranch("agent", agent =>
+        {
+            agent.SetDescription("CLI test agent for gRPC testing");
+            agent.AddCommand<TestAgentCommand>("agent")
+                .WithDescription("Test agent");
+        });
+    }
 }
--- nerv.log.Cli/Program.cs
+++ nerv.log.Cli/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using nerv.log.Cli;
 using Spectre.Console;
+using Spectre.Console.Cli;
 
 // nerv.log logo
 string logo = """
@@ -12,7 +14,13 @@ string logo = """
                  ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝  ╚═══╝ ╚═╝╚══════╝ ╚═════╝  ╚═════╝ 
                  """;
 
+var app = new CommandApp();
+
+app.Configure(CliConfig.ConfigureCommands);
+
 // welcome message
 AnsiConsole.MarkupLine($"[Red3_1]{logo}[/]");
 AnsiConsole.MarkupLine("Welcome to [Maroon]nerv.log v0.2[/] CLI tool!");
 AnsiConsole.MarkupLine("Type [Maroon]nerv help[/] for more information.");
+
+return await app.RunAsync(args);
