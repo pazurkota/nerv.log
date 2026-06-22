9c1ce82936a5452c772b06e307268cf83c590f72
pazurkota <piotrek.jelonek@icloud.com>
2026-06-22 13:26:18 +0200

    Add CLI project and initial implementation for nerv.log

---
+++ nerv.log.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Spectre.Console;
+
+// nerv.log logo
+string logo = """
+                 ███╗   ██╗███████╗██████╗ ██╗   ██╗  ██╗      ██████╗  ██████╗ 
+                 ████╗  ██║██╔════╝██╔══██╗██║   ██║  ██║     ██╔═══██╗██╔════╝ 
+                 ██╔██╗ ██║█████╗  ██████╔╝██║   ██║  ██║     ██║   ██║██║  ███╗
+                 ██║╚██╗██║██╔══╝  ██╔══██╗╚██╗ ██╔╝  ██║     ██║   ██║██║   ██║
+                 ██║ ╚████║███████╗██║  ██║ ╚████╔╝██╗███████╗╚██████╔╝╚██████╔╝
+                 ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝  ╚═══╝ ╚═╝╚══════╝ ╚═════╝  ╚═════╝ 
+                 """;
+
+// welcome message
+AnsiConsole.MarkupLine($"[Red3_1]{logo}[/]");
+AnsiConsole.MarkupLine("Welcome to [Maroon]nerv.log v0.2[/] CLI tool!");
+AnsiConsole.MarkupLine("Type [Maroon]nerv help[/] for more information.");
